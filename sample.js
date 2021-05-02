ef986e16d216b361cf24616c8f8077867f911eff
pauliclark <paul@venuescanner.com>
2021-05-02 14:04:38 +0100

    NPM package made public

--- sample.js
+++ sample.js
@@ -1,12 +1,9 @@
-import { eez, createEnvironment, identifiers, environments } from './index.js'
+import { eez, createEnvironment, identifiers } from './index.js'
 import controllers from './sample/controllers/index.js'
 const customEnvironment = 'myCustomEnv'
 createEnvironment(customEnvironment)
 eez({
   env: customEnvironment,
   port: identifiers.autoPort,
-  afterListen: () => {
-    controllers()
-    console.log(environments)
-  }
+  afterListen: controllers
 })
